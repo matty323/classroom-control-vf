5c4fd0a8701e33c9872b49944a70c5a0f46b0c95
Chris Roddy <cmroddy@gmail.com>
2016-03-28 13:59:21 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,5 +43,5 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
-  notify { "Github demo: success!": }
+  notify { "i just want this day to end": }
 }
