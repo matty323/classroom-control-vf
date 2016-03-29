2453a1f71b22cb0482c9242d1270cddeac8beb29
Matty <matt.larose@gmail.com>
2016-03-29 13:46:04 -0400

    Create init.pp

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,19 @@
+class memcached {
+
+  package { 'memcached':
+    ensure => 'installed',
+  }
+  
+  file { '/etc/sysconfig/memcached':
+      ensure  => file,
+      notify  => Service['memcached']
+      content => 'PORT="11211" USER="memcached" MAXCONN="96" CACHESIZE="32" OPTIONS=""',
+      }
+      
+  service { 'memcached':
+    ensure  => 'running',
+    enable  => true,
+    require => Package['memcached'],
+  }
+
+}
