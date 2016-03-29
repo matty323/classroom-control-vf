34f015fdffd87a6f1e86771d674f9fdf63a3e9a4
Matty <matt.larose@gmail.com>
2016-03-29 10:12:58 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,6 +42,7 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
+  include users
   notify { "Hello, my name is ${::hostname}": }
   notify { "Github demo: success!": }
   notify { "TRavis'd": }
